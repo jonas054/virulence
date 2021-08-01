34073a1a89d4b240ff8f0a5153b712ddda173c37
Jonas Arvidsson <jonas054@gmail.com>
2021-08-01 09:24:23 +0200

    Refactor Eraser

--- src/Eraser.java
+++ src/Eraser.java
@@ -2,19 +2,21 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 
 class Eraser {
+    private static final int HIDDEN = -1;
     private final int eraseRadius;
-    private int x = -1;
-    private int y = -1;
+    private int x = HIDDEN;
+    private int y = HIDDEN;
 
     Eraser(int eraseRadius) {
         this.eraseRadius = eraseRadius;
     }
 
     void draw(Graphics g, int squareSize) {
-        if (x != -1) {
+        if (isVisible()) {
             final int radiusInPixels = eraseRadius * squareSize;
             g.setColor(Color.white);
-            g.fillRect(x - radiusInPixels, y - radiusInPixels, 2 * radiusInPixels, 2 * radiusInPixels);
+            g.fillRect(x - radiusInPixels, y - radiusInPixels,
+                       2 * radiusInPixels, 2 * radiusInPixels);
         }
     }
 
@@ -24,10 +26,10 @@ class Eraser {
     }
 
     void hide() {
-        x = y = -1;
+        x = y = HIDDEN;
     }
 
     boolean isVisible() {
-        return x != -1;
+        return x != HIDDEN;
     }
 }
